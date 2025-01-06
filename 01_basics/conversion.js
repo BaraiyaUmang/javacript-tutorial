65373ce77782511e562429a51dd8bc61745c66dd
BaraiyaUmang <umangp610@gmail.com>
2025-01-06 11:22:34 +0000

    yet another code

---
+++ 01_basics/conversion.js
@@ -0,0 +1,30 @@
+// let score=true;
+
+// console.log(typeof score); // number
+// console.log(typeof (score)); // number
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // NaN
+
+
+// "33" -> 33
+//"33abc" -> NaN
+//true -> 1
+//false -> 0
+
+// let loggedIn = true;
+// let vari=Boolean(loggedIn);
+// console.log(vari); // true 
+
+
+//1 -> true
+//0 -> false
+//""=>false
+//"umang"=>true
+
+let number=24;
+let stringNumber=String(number);
+console.log(stringNumber); // "24"
+console.log(typeof stringNumber); // strings
