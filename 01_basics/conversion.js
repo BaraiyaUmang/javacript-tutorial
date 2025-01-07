67175b878a5e9781495270ba8cacf658368b79e5
BaraiyaUmang <umangp610@gmail.com>
2025-01-07 02:17:21 +0000

    added info

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -26,5 +26,58 @@
 
 let number=24;
 let stringNumber=String(number);
-console.log(stringNumber); // "24"
-console.log(typeof stringNumber); // strings
+// console.log(stringNumber); // "24"
+// console.log(typeof stringNumber); // strings
+
+
+let val = 3;
+let negval=-val;
+// console.log(negval);
+
+console.log(3+4); // 7
+console.log("3"+"4"); // 34
+console.log(3+"4"); // 34
+console.log(4-3); // 1
+console.log(6*8); // 48
+console.log(6/8); // 0.75
+console.log(6%8); // 6
+console.log(6**2); // 36
+
+let str1="Hello";
+let str2="World";
+let str3=str1+str2;
+console.log(str3); // HelloWorld   
+
+
+console.log("1"+2); // 12
+console.log(1+"2"); // 12
+console.log("1"+2+2); // 122
+console.log(1+2+"2"); // 32
+
+console.log(+true); // 1
+console.log(+false); // 0
+console.log(+null); // 0
+console.log(+undefined); // NaN
+console.log(+""); // 0
+
+let gameScore=100;
+gameScore++;
+console.log(gameScore); // 101
+
+
+let x = 3;
+const y = x++;
+
+console.log(x); // 4
+console.log(y); // 3
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+console.log(a); // 4
+console.log(b); // 4
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
