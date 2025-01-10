eecccf0710b36c1f8d8a743ed6a2e8bff0bdeecb
BaraiyaUmang <umangp610@gmail.com>
2025-01-10 04:54:15 +0000

    commit

---
+++ 05_iterations/two.js
@@ -0,0 +1,31 @@
+
+let i =0;
+while (i<=10) {
+    // console.log(i);
+    i+=2   
+}
+
+
+let myarray=["flash","bat","super"];
+
+let arr=0;
+while (arr<myarray.length) {
+    // console.log(myarray[arr]);
+    arr+=1;
+    
+}
+
+
+
+//do while loop  
+
+let score=11
+do {
+    console.log(score)            // here even if condition doesnot satisfy loop executes one time
+    score++                       // it is special case in loop here output will be 11 
+
+} while (score<=10);              //even if condition is false loop will executed one time because we do work first then checks the conditon
+
+
+
+
